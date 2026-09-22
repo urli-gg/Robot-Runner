13364b06d4c5b03ba4ee2dab3385ac78665a4b81
Gael <saucedo.gael1612@gmail.com>
2026-09-22 16:43:27 -0600

    Animaciones Popup

--- Assets/scripts/UIWindow.cs
+++ Assets/scripts/UIWindow.cs
@@ -1,14 +1,51 @@
+using DG.Tweening;
 using UnityEngine;
+using NaughtyAttributes;
 
 public class UIWindow : MonoBehaviour
 {
-    public virtual void Show()
+    [SerializeField] private RectTransform _canvasRectTransform;
+    [SerializeField] private CanvasGroup _canvasGroup;
+    [SerializeField] private bool _hideOnStart;
+    void Start()
     {
-
+        Initalize();
     }
 
-    public virtual void Hide()
+    public virtual void Initalize()
     {
-
+        if (_hideOnStart)
+        {
+            Hide();
+        }
     }
+
+    [Button]
+    public virtual void Show(bool instant = false)
+    {
+        if(instant)
+        {
+            _canvasRectTransform.gameObject.SetActive(true);
+        }
+        else
+        {
+            RectTransform rectTransform = _canvasGroup.GetComponent<RectTransform>();
+            rectTransform.DOScale(Vector3.one, duration :0.5f).SetEase(Ease.OutBack);
+        }
+    }
+
+    [Button]
+    public virtual void Hide(bool instant = false)
+    {
+        if (instant)
+        {
+            _canvasRectTransform.gameObject.SetActive(false);
+        }
+        else
+        {
+            RectTransform rectTransform = _canvasGroup.GetComponent<RectTransform>();
+            rectTransform.DOScale(Vector3.zero, duration: 0.5f).SetEase(Ease.InBack);
+        }
+    }
+
 }
